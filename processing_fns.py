c6a5cf27d89f15fb7e06029e39e2e69c35e874a6
laratt <laratt@umich.edu>
2025-04-10 13:19:31 -0400

    first commit

--- processing_fns.py
+++ processing_fns.py
@@ -3,6 +3,9 @@ import pandas as pd
 GAS_ABS = ['freq_GHz', 'dry_air_tau', 'wv_abs']
 LIQ_CLD = ['freq_GHz', 'mass_abs']
 
+GAS_ABS_FP = "/Users/laratobias-tarsh/Documents/wn25/clim485/project/mw_gas_absorption.txt"
+LIQ_CLD_FP = "/Users/laratobias-tarsh/Documents/wn25/clim485/project/liquid_cloud_ka_0degC.txt"
+
 def read_text_file(filepath,col_names=['freq_GHz', 'dry_air_tau', 'wv_abs']):
     """
     Reads text file for absorbption coefficient or optical depths
